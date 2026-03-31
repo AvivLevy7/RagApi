78c52481126da16444423ff742ab43209b4b960d
aviv <aviv02034@gmail.com>
2026-03-31 14:46:01 +0300

    shallow api

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+from fastapi import FastAPI, UploadFile, File
+import pypdf
+import io
+
+app = FastAPI()
+
+@app.post("/upload")
+async def upload_pdf(file: UploadFile = File(...)):
+    contents = await file.read()
+    pdf_reader = pypdf.PdfReader(io.BytesIO(contents))
+
+    extracted_text = ""
+    for page in pdf_reader.pages:
+        extracted_text += page.extract_text() + "\n"
+
+    print(f"--- הטקסט שחולץ ---"[::-1])
+    print(extracted_text[:500][::-1])
+
+    return {"filename": file.filename, "status": "Success"}
 
 
